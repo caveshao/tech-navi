4165a3d777ce1c5e5e787364942f546a2de9eea1
caveshao <caveshao@gmail.com>
2021-03-08 23:45:41 +0800

    dev: add engine switch module

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
 import { Input, Switch, Row, Col } from "antd";
 import { AudioOutlined } from "@ant-design/icons";
+
+import styles from "./SearchBar.module.less";
+
+export interface SearchProps {
+  placeholder?: string;
+}
+
+// type engine = "google" | "baidu";
+enum Engine {
+  google = "google",
+  baidu = "baidu",
+}
+
 const { Search } = Input;
 const suffix = (
   <AudioOutlined
@@ -9,29 +23,35 @@ const suffix = (
     }}
   />
 );
-export interface SearchProps {
-  placeholder?: string
-}
 
-const SearchBar = ({placeholder}: SearchProps) => {
-  const onSearch = () => {
-    console.log("search");
-  };
+const SearchBar = ({ placeholder }: SearchProps) => {
+  const [searchEngine, setSearchEngine] = useState<Engine>(Engine.baidu);
+  const onEngineChange = (checked: boolean) =>
+    setSearchEngine(checked ? Engine.google : Engine.baidu);
+  const onSearch = () => {};
   return (
-    <Row justify="center" align="middle">
-      <Col span={1}>
-        <Switch />
-      </Col>
-      <Col span={7}>
-        <Search
-          placeholder={placeholder}
-          enterButton="Search"
-          size="middle"
-          suffix={suffix}
-          onSearch={onSearch}
-        />
-      </Col>
-    </Row>
+    <>
+      <h1 className={styles.title}>{searchEngine}</h1>
+      <Row justify="center" align="middle">
+        <Col span={1}>
+          <Switch
+            checkedChildren={Engine.google}
+            unCheckedChildren={Engine.baidu}
+            defaultChecked={false}
+            onChange={onEngineChange}
+          />
+        </Col>
+        <Col span={7}>
+          <Search
+            placeholder={placeholder}
+            enterButton="Search"
+            size="middle"
+            suffix={suffix}
+            onSearch={onSearch}
+          />
+        </Col>
+      </Row>
+    </>
   );
 };
 
