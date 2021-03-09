305738574a57d1490cc1478bb66d05b2ce37da42
caveshao <caveshao@gmail.com>
2021-03-09 22:11:53 +0800

    feat: add settting handle

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { Button } from "antd";
 import { SettingOutlined } from "@ant-design/icons";
-import styles from "./Setting.module.less"
+import styles from "./Setting.module.less";
 
 const Setting = () => {
+  const [setting, setSettingStatus] = useState<boolean>(false);
+  const handleClick = () => {
+      setSettingStatus(!setting)
+  }
   return (
     <div className={styles.setting}>
-      <Button type="text" size="large">
+      <Button type="text" size="large" onClick={handleClick}>
         <SettingOutlined />
-      </Button>
+       </Button>
     </div>
   );
 };
